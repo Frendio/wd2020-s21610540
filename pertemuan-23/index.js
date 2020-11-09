315ed88f128c804bf6bfaa619c941e24de3314a1
Frendio <frendio98@gmail.com>
2020-11-09 22:01:34 +0800

    Basic Javascript & Exercise 7

---
+++ pertemuan-23/index.js
@@ -0,0 +1,50 @@
+//variabel
+//let & const
+// let umur = 25;
+// let nilai = 95.25;
+// let sudahMenikah = false; //bolean
+// let nama = "John"; //string
+// console.log(umur);
+// console.log(nama);
+
+// let alamat;
+// console.log(alamat); //undefined
+// const tahunLahir = 2020;
+// console.log(tahunLahir);
+      
+//Operator
+
+//Aritmatic Operator(+,-,*,:)
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let namaDepan = "John";
+// let namaBelakang = "seber";
+// console.log("Nama Saya" + namaDepan + " "+ namaBelakang + ". Umur saya adalah" + umur + "Tahun"
+// );
+
+// //operator logika
+// let kondisi1 = 1<5;
+// let kondisi2 = true;
+
+// console.log(kondisi1 || kondisi2);
+// console.log(!log)
+
+//Operator Perbandingan
+// let angka1 = 10;
+// let angka2 = 8";
+ 
+// console.log(angka1 === angka2);
+//Ternary Operator
+// let angka = 5;
+// let hasil = angka < 0 ? "Negatif" : "Positif";
+//     console.log(hasil);
+
+
+
+
+let berat = prompt("Input berat anda = kg");
+let tinggi = prompt("Input tinggi anda = cm");
+let bni = berat / (tinggi*tinggi);
+console.log("BMI = " + bni);
